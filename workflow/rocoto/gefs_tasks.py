b1106b9cd286b913ad35a8e14861714641d3d823
NeilBarton-NOAA <neil.barton@noaa.gov>
2024-02-07 21:08:20 +0000

    initial changes for EP5a ICs

--- workflow/rocoto/gefs_tasks.py
+++ workflow/rocoto/gefs_tasks.py
@@ -29,15 +29,16 @@ class GEFSTasks(Tasks):
         if self.app_config.do_ocean:
             ocn_res = f"{self._base.get('OCNRES', '025'):03d}"
             prefix = f"{cpl_ic['BASE_CPLIC']}/{cpl_ic['CPL_OCNIC']}/@Y@m@d@H/mem000/ocean"
-            data = f"{prefix}/@Y@m@d.@H0000.MOM.res.nc"
+            #data = f"{prefix}/@Y@m@d.@H0000.MOM.res.nc"
+            data = f"{prefix}/ORAS5.mx{ocn_res}.ic.nc"
             dep_dict = {'type': 'data', 'data': data}
             deps.append(rocoto.add_dependency(dep_dict))
-            if ocn_res in ['025']:
-                # 0.25 degree ocean model also has these additional restarts
-                for res in [f'res_{res_index}' for res_index in range(1, 4)]:
-                    data = f"{prefix}/@Y@m@d.@H0000.MOM.{res}.nc"
-                    dep_dict = {'type': 'data', 'data': data}
-                    deps.append(rocoto.add_dependency(dep_dict))
+            #if ocn_res in ['025']:
+            #    # 0.25 degree ocean model also has these additional restarts
+            #    for res in [f'res_{res_index}' for res_index in range(1, 4)]:
+            #        data = f"{prefix}/@Y@m@d.@H0000.MOM.{res}.nc"
+            #        dep_dict = {'type': 'data', 'data': data}
+            #        deps.append(rocoto.add_dependency(dep_dict))
 
         # Ice ICs
         if self.app_config.do_ice:

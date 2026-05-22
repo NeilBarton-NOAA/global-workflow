7d300963b0a61886635c72b51697459f190ee905
NeilBarton-NOAA <neil.barton@noaa.gov>
2026-05-22 14:15:59 +0000

    updates to run atmo_post on wcoss2

--- ush/process_atmos_daily.sh
+++ ush/process_atmos_daily.sh
@@ -97,7 +97,8 @@ dest_inst="${OUTDIR}/inst.daily.${MEMDIR}/inst.daily.${filename_start}${filemm}$
 if [ -d "${tmp_acc_work_dir}" ]; then
     # Clear array and fill it using mapfile
     unset acc_files
-    mapfile -t acc_files < <(ls -v "${tmp_acc_work_dir}"/daily_acc*.grb 2>/dev/null)
+    acc_string=$(ls -v "${tmp_acc_work_dir}"/daily_acc*.grb 2>/dev/null)
+    mapfile -t acc_files <<< "${acc_string}"
     
     if [ ${#acc_files[@]} -gt 0 ]; then
         echo "INFO: Task $i merging ${#acc_files[@]} days for ACC using array expansion."
@@ -110,8 +111,10 @@ fi
 
 # 2. Consolidate Instantaneous (INST)
 if [ -d "${tmp_inst_work_dir}" ]; then
+    # Clear array and fill it using mapfile
     unset inst_files
-    mapfile -t inst_files < <(ls -v "${tmp_inst_work_dir}"/daily_inst*.grb 2>/dev/null)
+    inst_string=$(ls -v "${tmp_inst_work_dir}"/daily_inst*.grb 2>/dev/null)
+    mapfile -t inst_files <<< "${inst_string}"
     
     if [ ${#inst_files[@]} -gt 0 ]; then
         echo "INFO: Task $i merging ${#inst_files[@]} days for INST using array expansion."
